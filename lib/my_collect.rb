e3cf6cb9218ab46624d58e3ac1f9319f89ba90c2
Xavier Perez <xperez007@gmail.com>
2020-01-28 13:52:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,4 +4,10 @@ my_collection(array) do|name|
   name.split(" ").first 
 end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collection(collection) do
+|lang|
+  lang.upcase
+end
+
 
